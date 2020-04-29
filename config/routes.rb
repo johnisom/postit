54bf0c6c542577647975c3c0790c2cbe57ac2b6d
John Isom <John.Isom2001@gmail.com>
2020-04-29 13:16:28 -0600

    Add comments routes nested under posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: %i[destroy]
+  resources :posts, except: %i[destroy] do
+    resources :comments, only: %i[create]
+  end
+
   resources :categories, only: %i[new create show]
 end
