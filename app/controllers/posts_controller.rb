e049c914acf6c16db78c8bc8a007325d7b1427ee
John Isom <John.Isom2001@gmail.com>
2020-04-28 18:23:34 -0600

    Add ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,31 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first # TODO: make this actual LOL
+
+    if @post.save
+      flash[:notice] = 'Post successfully updated'
+      redirect_to posts_path
+    else
+      flash[:error] = 'Post could not be updated'
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
