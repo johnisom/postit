f02d590d1af8afc6dbad661b6c84b8b1ce94a568
John Isom <John.Isom2001@gmail.com>
2020-05-03 13:49:01 -0600

    Require that users are required to be signed in for important actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :require_user, except: %i[show index]
   protect_from_forgery with: :exception
 
   helper_method :current_user, :logged_in?
@@ -11,5 +10,14 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  private
+
+  def require_user
+    if !logged_in?
+      flash[:error] = 'You must be logged in to do that.'
+      redirect_to root_path
+    end
   end
 end
