8b4cd5b84e200e6bca365efa961db4040d1f5cbe
John Isom <John.Isom2001@gmail.com>
2020-05-04 08:52:35 -0600

    Finish user features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :require_same_user, only: %[edit update]
 
   def new
     @user = User.new
@@ -40,5 +41,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
+    end
   end
 end
